612936bcb9f67c8785f4bd1ffb7c2bb1861a86bb
Nishant <nishant.mogha@firewires.in>
2022-09-28 20:49:30 +0530

    add next wallpaper caching for instant loading of new tab

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,22 @@ import clsx from "clsx"
 import { useSettingStore } from "./store/settings"
 import ThemeToggle from "./components/ThemeToggle"
 import Footer from "./components/Footer"
+import { getRandomImage } from "./utils/image"
+import { useMemo } from "preact/hooks"
 
 export default function App() {
   const darkMode = useSettingStore.darkMode()[0]
+
+  const randImg = useMemo(
+    () => getRandomImage({ seeds: ["wallpaper", "nature", "abstract"] }),
+    []
+  )
 
   return (
     <main
       class={clsx("h-screen bg-cover bg-center", darkMode && "dark")}
       style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.1)), url(https://source.unsplash.com/random/${
-          window.screen.width + "x" + window.screen.height
-        }?wallpaper,nature,abstract)`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.1)), url("${randImg}")`,
       }}
     >
       <ThemeToggle className="absolute right-2 top-2" />
